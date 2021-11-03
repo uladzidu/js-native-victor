680b433b113a79e83e8788f22f59b23bf99777f6
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-11-03 14:50:18 +0100

    compleated tests

--- JS-Course/src/02-test/02_02.ts
+++ JS-Course/src/02-test/02_02.ts
@@ -1,8 +1,35 @@
+import { type } from "os"
+
+export type StreetType = {
+    title: string
+}
+export type AdressType = {
+    number: number
+    street: StreetType
+}
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    adress: AdressType
+}
 export type CityType = {
-    title : string
-    houses : Array<string>
-    governmentBuildings : Array<string>
-    citizensNumber : number
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<GoverType>
+    citizensNumber: number
 }
 
+
+export type StreetGovType = {
+    title : string
+}
+export type AdressGovType = {
+    street : StreetGovType
+}
+export type GoverType = {
+    type : "HOSPITAL" | "FIRE-STATION"
+    budget : number
+    staffCount : number
+    adress : AdressGovType
+}
 
