58d6557917643a125df45fab66abbf54e3384b28
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-11-07 19:05:05 +0100

    07.11. Testing WebStorm App_2

--- JS-Course/src/index.tsx
+++ JS-Course/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { splitIntoWords } from './01-hello-test/01';
-import { sum } from './03-test/03';
+import {sum} from './03-test/03';
 import {App} from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -9,10 +8,7 @@ import * as serviceWorker from './serviceWorker';
 var a = sum;
 
 
-
-
-
-ReactDOM.render(<App/>,  document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
