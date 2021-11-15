33d860bc2a0b47e992a65123e7a4b80e842f47ff
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-11-15 10:29:42 +0100

    15.11. Added 04_02 test

--- JS-Course/src/04-test/04.test.ts
+++ JS-Course/src/04-test/04.test.ts
@@ -1,25 +1,22 @@
-test.skip ("should take man older than 90", () => {
+// @ts-ignore
+test.skip('should take man older than 90', () => {
 
-    const ages = [14,18,45,100,28,95];
+    const ages = [14, 18, 45, 100, 28, 95];
 
-    const oldAges = ages.filter((age : number) => age > 90);
+    const oldAges = ages.filter((age: number) => age > 90);
 
     expect(oldAges.length).toBe(2)
     expect(oldAges[0]).toBe(100)
     expect(oldAges[1]).toBe(95)
 })
 
-test ("should take course chipper than 160$ ", () => {
+test.skip('should take course chipper than 160$ ', () => {
 
-    type CourseType = {
-        title : string
-        price : number
-    }
 
     const courses = [
-        { title : "CSS", price : 110},
-        { title : "JS", price : 200},
-        { title : "HTML", price : 150},
+        {title: 'CSS', price: 110},
+        {title: 'JS', price: 200},
+        {title: 'HTML', price: 150},
     ];
 
 
@@ -28,3 +25,37 @@ test ("should take course chipper than 160$ ", () => {
     expect(filteredArray.length).toBe(2)
     expect(filteredArray[0].price).toBe(110)
 })
+
+test.skip('get only completed tasks', () => {
+
+    const tasks = [
+        {id: 1, title: 'bread', isDone: false},
+        {id: 2, title: 'milk', isDone: true},
+        {id: 3, title: 'salt', isDone: false},
+        {id: 4, title: 'sugar', isDone: true},
+        {id: 5, title: 'tea', isDone: false},
+    ]
+
+    const filteredTasks = tasks.filter(task => task.isDone)
+
+    expect(filteredTasks.length).toBe(2)
+    expect(filteredTasks[0].title).toBe('milk')
+})
+
+
+test('get only uncompleted tasks', () => {
+
+    const tasks = [
+        {id: 1, title: 'bread', isDone: false},
+        {id: 2, title: 'milk', isDone: true},
+        {id: 3, title: 'salt', isDone: false},
+        {id: 4, title: 'sugar', isDone: true},
+        {id: 5, title: 'tea', isDone: false},
+    ]
+
+    const uncompletedTasks = tasks.filter(task => !task.isDone)
+
+    expect(uncompletedTasks.length).toBe(3)
+    expect(uncompletedTasks[0].title).toBe('bread')
+})
+
---
+++ JS-Course/src/04-test/04_02.test.ts
@@ -0,0 +1,65 @@
+import {CityType} from '../02-test/02_02';
+
+
+let city: CityType;
+
+beforeEach(() => {
+    city = {
+        title: "New York",
+        houses: [
+            {
+                buildedAt: 2012,
+                repaired: false,
+                adress: {
+                    number: 100,
+                    street: {
+                        title: "White street"
+                    }
+                }
+            },
+            {
+                buildedAt: 2008,
+                repaired: false,
+                adress: {
+                    number: 100,
+                    street: {
+                        title: "Happy street"
+                    }
+                }
+            },
+            {
+                buildedAt: 2020,
+                repaired: false,
+                adress: {
+                    number: 101,
+                    street: {
+                        title: "Happy street"
+                    }
+                }
+            }
+        ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL",
+                budget: 200000,
+                staffCount: 200,
+                adress: {
+                    street: {
+                        title: "Central Str"
+                    }
+                }
+            },
+            {
+                type: "FIRE-STATION",
+                budget: 500000,
+                staffCount: 1000,
+                adress: {
+                    street: {
+                        title: "South Str"
+                    }
+                }
+            }
+        ],
+        citizensNumber: 1000000
+    }
+})
