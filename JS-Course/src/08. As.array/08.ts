986d016326273f1267cde289a77e76f9f88666aa
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-12-18 09:42:30 +0100

    20.11

---
+++ JS-Course/src/08. As.array/08.ts
@@ -0,0 +1,13 @@
+export const users = {
+    '101': {id: 101, name: 'Dimych'},
+    '113231': {id: 113231, name: 'Dimych'},
+    '321312': {id: 321312, name: 'Dimych'},
+    '1': {id: 1, name: 'Dimych'},
+}
+
+export const usersArray = [
+    {id: 101, name: 'Dimych'},
+    {id: 113231, name: 'Dimych'},
+    {id: 321312, name: 'Dimych'},
+    {id: 1, name: 'Dimych'},
+]
